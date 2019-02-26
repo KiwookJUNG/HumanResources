fba741390bf57261262552570846d113154b19f1
정기욱 <rldnr56@me.com>
2019-02-26 16:02:11 +0900

    인사관리앱 구현을 위해 FMDB라이브러리 반입

---
+++ HumanResources/DepartmentListVC.swift
@@ -0,0 +1,14 @@
+//
+//  DepartmentListVC.swift
+//  HumanResources
+//
+//  Created by 정기욱 on 26/02/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import UIKit
+
+class DpeartmentListVC : UITableViewController {
+    
+    
+}
