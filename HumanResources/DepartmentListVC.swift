1a6763754ca9f023ba5fd41e8a6c4ccd5cac9b0f
정기욱 <rldnr56@me.com>
2019-02-27 13:44:07 +0900

    1. 멤버 변수인 departList는 테이블 뷰의 데이터 소스를 담당할 배열이다. DepartmentDAO 클래스의 find 메소드가 반환하는 값을 받아 저장해야 하므로 동일한 튜플 타입의 형식으로 정의해준다.
    2. 다음으로 departDAO변수를 선언하고 DepartmentDAO인스턴스로 초기화 한다.
    
    3. initUI메소드는 네이게이션 타이틀에 들어갈 레이블 객체를 생성하고 관련된 속성을 설정해준다.
    
    4. 네비게이션 바 UI를 설정해 주는데, 3에서 정의한 레이블을 네비게이션 바에 넣어주고, 왼쪽바버튼 아이템에 편집버튼을 추가해준다.
    editButtonItem은 누르면 모든 셀을 편집할 수 있는 아이템으로 프로그래밍 방식에서만 제공됨.
    
    5. 셀을 왼쪽으로 스와이프하여 해당 셀만 편집모드로 전환되도록 설정할 수 있다. (allowsSelectionDuringEditing)

--- HumanResources/DepartmentListVC.swift
+++ HumanResources/DepartmentListVC.swift
@@ -10,5 +10,23 @@ import UIKit
 
 class DpeartmentListVC : UITableViewController {
     
+    var departList: [(departCd: Int, departTitle: String, departAddr: String)]! // 데이터 소스용 멤버 변수
+    let departDAO = DepartmentDAO() // SQLite 처리를 담당할 DAO 객체
     
+    // UI 초기화 함수
+    func initUI() {
+        // 1. 내비게이션 타이틀용 레이블 속성 설정
+        let navTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 14)
+        navTitle.text = "부서 목록 \n" + " 총 \(self.departList.count) 개"
+        
+        // 2. 내비게이션 바 UI 설정
+        self.navigationItem.titleView = navTitle
+        self.navigationItem.leftBarButtonItem = self.editButtonItem // 편집 버튼 추가
+        
+        // 3. 셀을 스와이프 했을 때 편집모드가 되도록 설정
+        self.tableView.allowsSelectionDuringEditing = true
+    }
 }
