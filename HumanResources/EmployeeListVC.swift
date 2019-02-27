7ccdec1e42fdd85ba2094bd51d7c1fbbc52a14bb
정기욱 <rldnr56@me.com>
2019-02-27 14:54:53 +0900

    1. 데이터 소스를 저장할 empList 변수 및 EmployeeDAO객체를 저장할 empDAO변수 초기화
    2. initUI 메소드 및 viewDidLoad 메소드 구현

--- HumanResources/EmployeeListVC.swift
+++ HumanResources/EmployeeListVC.swift
@@ -10,5 +10,26 @@ import UIKit
 
 class EmployeeListVC : UITableViewController {
     
+    // 데이터 소스를 저장할 멤버변수
+    var empList: [EmployeeVO]!
+    // SQLite 처리를 담당할 DAO 클래스
+    var empDAO = EmployeeDAO()
+    
+    // UI를 초기화할 메소드 initUI를 정의
+    func initUI() {
+        // 내비게이션 타이틀용 레이블 속성 설정
+        let navTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 14)
+        navTitle.text = "사원 목록 \n" + " 총 \(self.empList.count) 명"
+        
+        self.navigationItem.titleView = navTitle
+    }
+    
+    override func viewDidLoad() {
+        self.empList = self.empDAO.find()
+        self.initUI()
+    }
     
 }
