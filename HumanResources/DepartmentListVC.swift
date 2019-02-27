baf9c79f3c988a517160157e5505d9fbdcae913f
정기욱 <rldnr56@me.com>
2019-02-27 14:04:02 +0900

    1. viewDidLoad 메소드에 먼저 기존 저장된 부서정보를 가져온다 ( departDAO 객체 사용 즉, 데이터베이스에 있는 정보를 가져온다.) 이후 내비게이션 UI를 실행해준다.
    2. tableView의 셀 갯수를 반환한다.
    
    3. cellForRowAt 매개 변수가 있는 tableView 메소드를 이용해 데이터를 배치해준다.

--- HumanResources/DepartmentListVC.swift
+++ HumanResources/DepartmentListVC.swift
@@ -29,4 +29,30 @@ class DpeartmentListVC : UITableViewController {
         // 3. 셀을 스와이프 했을 때 편집모드가 되도록 설정
         self.tableView.allowsSelectionDuringEditing = true
     }
+    
+    override func viewDidLoad() {
+        self.departList = self.departDAO.find() // 기존 저장된 부서정보를 가져온다.
+        // 반드시 initUI보다 먼저 실행되야함. initUI 메소드 내부에서 departList를 참조하기 때문이다.
+        self.initUI()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.departList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // indexPath 매개변수가 가리키는 행에 대한 데이터를 읽어온다.
+        let rowData = self.departList[indexPath.row]
+        
+        // 셀 객체를 생성하고 데이터를 배치한다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DEPART_CELL")
+        
+        cell?.textLabel?.text = rowData.departTitle
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        
+        cell?.detailTextLabel?.text = rowData.departAddr
+        cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 12)
+        
+        return cell!
+    }
 }
